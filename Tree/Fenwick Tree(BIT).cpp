ac38ce79c1db3457954eaf820fc97eae8c049659
evelyn82ny <evelyn82ny@gmail.com>
2021-08-30 11:27:54 +0900

    Modify BIT code

--- Tree/Fenwick Tree(BIT).cpp
+++ Tree/Fenwick Tree(BIT).cpp
@@ -1,16 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 vector<long long> fw;
 
 long long sum(int pos) {
+
 	long long ret = 0;
 	while (pos > 0) {
 		ret += fw[pos];
 		pos -= (pos & -pos);
+		// pos &= (pos - 1);
 	}
 	return ret;
 }
 
-void add(int pos, long long diff) {
-	while (pos < tree.size()) {
+void update(int pos, long long diff) {
+	
+	while (pos < fw.size()) {
 		fw[pos] += diff;
 		pos += (pos & -pos);
 	}
